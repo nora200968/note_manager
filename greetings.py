81c2a1af2cc4a617b95c445b7830467a50153f40
nora200968 <nora20.09@mail.ru>
2025-01-26 20:36:45 +0200

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,12 @@
+username = 'Введите имя пользователя: ' # username: Имя пользователя
+print(username)
+title = 'Введите заголовок заметки: '   # title: Заголовок заметки;
+print(title)
+content = 'Введите описание заметки: '  # content: Описание заметки;
+print(content)
+status = 'Введите статус заметки: '     # status: Статус заметки;
+print(status)
+created_date = 'Введите дату создания заметки в формате "день-месяц-год": 20-01-2025 '    # created_date: Дата создания заметки;
+print(created_date)
+issue_date = 'Введите дату истечения заметки в формате "день-месяц-год": 20-01-2025 '      # issue_date: Дата истечения заметки;
+print(issue_date)
