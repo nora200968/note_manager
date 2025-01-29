7612cb21cf60ec686989d08826120f49aee93fa7
nora200968 <nora20.09@mail.ru>
2025-01-29 23:36:38 +0200

    Add files via upload

---
+++ add.input.py
@@ -0,0 +1,18 @@
+username = input('Введите имя пользователя: ') # username: Имя пользователя
+print(username)
+title = input('Введите заголовок заметки: ')   # title: Заголовок заметки;
+print(title)
+content = input('Введите описание заметки: ')  # content: Описание заметки;
+print(content)
+status = input('Введите статус заметки: ')     # status: Статус заметки;
+print(status)
+created_date = 'Введите дату создания заметки в формате "день-месяц-год": 20-01-2025 '    # created_date: Дата создания заметки;
+#print(created_date)
+temp_created_data = input('Введите дату создания заметки в формате: DD-MM ')
+print(temp_created_data)
+#дата создания заметки для ввода пользователем
+issue_date = 'Введите дату истечения заметки в формате "день-месяц-год": 20-01-2025 '      # issue_date: Дата истечения заметки;
+#print(issue_date)
+temp_issue_date = input('Введите дату истечения заметки в формате: DD-MM ')
+print(temp_issue_date)
+#дата истечения заметки для ввода пользователем
